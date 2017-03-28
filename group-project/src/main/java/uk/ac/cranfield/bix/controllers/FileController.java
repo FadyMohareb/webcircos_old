62ba74b29c200f86286b2adde33694f3ee23425e
s260533 <s260533@LIW7108F.cns.cranfield.ac.uk>
2017-03-28 11:06:02 +0100

    Folders with file type added

--- group-project/src/main/java/uk/ac/cranfield/bix/controllers/FileController.java
+++ group-project/src/main/java/uk/ac/cranfield/bix/controllers/FileController.java
@@ -1,24 +1,17 @@
 package uk.ac.cranfield.bix.controllers;
 
+import java.io.BufferedWriter;
 import java.io.File;
-import java.io.IOException;
+import java.io.FileWriter;
 import java.nio.file.Files;
-import java.nio.file.Path;
-import static java.nio.file.StandardCopyOption.*;
-import java.util.Iterator;
-import java.util.LinkedList;
-import javax.servlet.http.HttpServletResponse;
-import org.apache.tomcat.util.http.fileupload.IOUtils;
+import org.springframework.security.authentication.AnonymousAuthenticationToken;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.context.request.RequestContextHolder;
-import org.springframework.web.multipart.MultipartFile;
-import org.springframework.web.multipart.MultipartHttpServletRequest;
 import uk.ac.cranfield.bix.controllers.rest.NewFile;
 import uk.ac.cranfield.bix.controllers.rest.RestResponse;
 
@@ -28,32 +21,49 @@ public class FileController {
     public
     @ResponseBody
     RestResponse upload(@RequestBody(required = true) NewFile inputFile) {
+        //initializations
         String filePath, fileName, currentPath, fileType, newPath="", userID;
-        File oldFile, newFile, dir;
-        
+        File oldFile, newFile, dir1, dir2;
+        FileWriter fileWriter;
+        BufferedWriter bufferedWriter;
         
         try {
+            //information about old file to copy
             filePath = inputFile.getFilePath();
             fileType = inputFile.getFileType();
             fileName = inputFile.getFileName();
+            //creating object file from path
             oldFile = new File(filePath);
+            //current path THE ONE TO CHANGE AT DIFFERENT COMPUTERS
             currentPath = ("Z:/ProfileData/s260533/Desktop/temp/");
-//            if (SecurityContextHolder.getContext().getAuthentication().isAuthenticated())
-//            {
-//                userID = SecurityContextHolder.getContext().getAuthentication().getName();
-//            }
-//            else
-//            {
+            //user id from SpringSecurity
+            userID = SecurityContextHolder.getContext().getAuthentication().getName();
+            //temporary solution
+            if ("anonymousUser".equals(userID))
+            {
                 userID = RequestContextHolder.currentRequestAttributes().getSessionId();
-//            }
+            }
+            //newPath with added userID
             newPath=(currentPath+userID);
-            dir = new File(newPath);
-            dir.mkdir();
-            newPath=newPath+"/"+fileName;
-            newFile = new File(newPath);
-//            Files.copy(oldFile, newFile);
+            dir1 = new File(newPath);
+            //creating new directory for user if doesn't exist
+            if (!dir1.exists())
+                dir1.mkdir();
+            //newPath with added FileType
+            newPath=(newPath+"/"+fileType);
+            dir2 = new File(newPath);
+            //creating new directory for user if doesn't exist
+            if (!dir2.exists())
+                dir2.mkdir();
+            //copy file to new directory
+            newFile = new File(newPath+"/"+fileName);
             Files.copy(oldFile.toPath(), newFile.toPath());
-                   
+            //new file
+            fileWriter = new FileWriter(newPath+"/contentOfFolder.txt");
+            bufferedWriter = new BufferedWriter(fileWriter);
+            bufferedWriter.write(fileName);
+            bufferedWriter.close();
+            fileWriter.close();
             return new RestResponse(null, null);
         } catch (Exception e) {
             return new RestResponse(e.getMessage(), null);
