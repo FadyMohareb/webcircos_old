1b8b511e9b0165c2a69c35f1bcc3672f3e85de42
s260592 <s260592@LIW7107F.cns.cranfield.ac.uk>
2017-04-09 21:52:42 +0100

    Displaying files list as a dropdown list - need to manage also changing currently active files names on the button, for now setting button name automatically works only for FASTA but list id rendered also for annotation.

--- group-project/src/main/webapp/resources/js/panels/filesGeneralPanel.js
+++ group-project/src/main/webapp/resources/js/panels/filesGeneralPanel.js
@@ -2,25 +2,48 @@
 
 var converter = new Showdown.converter();
 
-var ProperFilesListRender = React.createClass({displayName: "ProperFilesListRender",
-    
-    render: function() {
-      return (React.createElement("div", null,
-            this.props.list.map(function(listValue)
-            {console.log('FILES NAMES: ' + listValue)
-                return (React.createElement("h5",null," "+listValue+"\n"));
-                //checkbox instead of line
-//                return (React.createElement("input", { type: 'checkbox' }, " "+listValue+"\n"));
-            })
-        )
-      );
-    }
-  });
-  
-var FilesGeneralPanel = React.createClass({className: "FilesGeneralPanel",
-
+var FilesDropdown = React.createClass({displayName: "FilesDropdown",
     getInitialState: function getInitialState() {
-        return {view: {showUploadModal: false} };
+        return {activeFile: this.props.filesList[0]}
+    },
+    renderFilesBlock: function renderBlock(filesList, parent) {
+
+        return filesList.map(function (fileName)
+        {
+            handleFileChange: function handleFileChange(event) {
+                
+                event.preventDefault();
+                parent.state.activeFile = event.target.id;
+//                var btnId = parent.props.fileType + 'Btn';
+//                console.log(btnId);
+//                console.log('Active file: ' + parent.state.activeFile);
+//                $('#btnId').children().first().text(parent.state.activeFile + ' ');
+
+            }
+            ;
+
+            return (React.createElement("li", {onClick: handleFileChange, id: fileName}, fileName));
+        })
+    },
+    render: function () {
+        var btnId = this.props.fileType + 'Btn';
+            
+        return React.createElement('div', {className: 'btn-group'},
+                React.createElement('button', {className: 'btn btn-default dropdown-toggle', 'data-toggle': "dropdown", 'aria-haspopup': "true", 'aria-expanded': 'false'}, this.state.activeFile + ' ',
+                        React.createElement('span', {className: 'caret'})),
+                React.createElement('ul', {className: 'dropdown-menu'},
+                        this.renderFilesBlock(this.props.filesList, this))
+                )
+    }
+
+
+
+});
+
+
+var FilesGeneralPanel = React.createClass({className: "FilesGeneralPanel",
+    getInitialState: function getInitialState() {
+        return {view: {showUploadModal: false}};
     },
     handleShowUploadModal: function handleShowUploadModal() {
         this.setState({view: {showUploadModal: true}});
@@ -29,7 +52,7 @@ var FilesGeneralPanel = React.createClass({className: "FilesGeneralPanel",
         this.setState({view: {showUploadModal: false}});
         $(".modal-backdrop.in").remove();
     },
-        contentUpdateProject: function(panelType)
+    contentUpdateProject: function (panelType)
     {
         var projectName = this.props.projectName;
         var fd = new FormData();
@@ -42,42 +65,43 @@ var FilesGeneralPanel = React.createClass({className: "FilesGeneralPanel",
             processData: false,
             contentType: false,
             data: fd,
-            success: function(data) 
+            success: function (data)
             {
                 var filesList = data.errors;
                 if (filesList !== "")
                 {
-                    var substring0=filesList.substring(0,1);
-                    if (substring0==="[")
+                    var substring0 = filesList.substring(0, 1);
+                    if (substring0 === "[")
                     {
-                        filesList = filesList.substring(1,filesList.length-1);
+                        filesList = filesList.substring(1, filesList.length - 1);
                         var filesSplited = filesList.split(",");
                         var n = filesSplited.length;
                         var list = [];
                         for (var i = 0; i < n; i++)
                         {
-                            list[i]=filesSplited[i];
+                            list[i] = filesSplited[i];
                         }
-                    }
-                    else
+                    } else
                     {
                         var filesSplited = filesList.split("\t");
                         var list = [];
                         for (var i = 0; i < filesSplited.length; i++)
                         {
-                            if (filesSplited[i]!=="")
+                            if (filesSplited[i] !== "")
                             {
                                 var filesSplited2 = filesSplited[i].split("/");
-                                var fileName = filesSplited2[filesSplited2.length-1];
-                                list[i]=fileName;
+                                var fileName = filesSplited2[filesSplited2.length - 1];
+                                list[i] = fileName;
                             }
                         }
-                        
-                        
+
+
                     }
-                    
-                    if (list.length >0)
-                        return React.render(React.createElement(ProperFilesListRender, {list: list}), document.getElementById(type));
+
+                    if (list.length > 0)
+                        return React.render(React.createElement('div', {className: 'container'},
+                                React.createElement(FilesDropdown, {filesList: list, fileType: type})),
+                                document.getElementById(type));
                 }
             },
             error: function (status, err) {
@@ -87,37 +111,29 @@ var FilesGeneralPanel = React.createClass({className: "FilesGeneralPanel",
     },
     render: function () {
         return (React.createElement('div', {className: "container"},
-        
-                        React.createElement('div', {className: "panel panel-success"},
-                                React.createElement('div', {className: "panel-heading"}, "Sequence"),
-                                    React.createElement('div', {className: "panel-body"},
-                                        React.createElement('div', {className: 'container', id: 'sequence'},
-                                            React.createElement(this.contentUpdateProject, {panelType: "sequence"})))),
-                        React.createElement('div', {className: "panel panel-info"},
-                                React.createElement('div', {className: "panel-heading"}, "Alignment"),
-                                    React.createElement('div', {className: "panel-body"},
-                                        React.createElement('div', {className: 'container', id: 'alignment'},
-                                            React.createElement(this.contentUpdateProject, {panelType: "alignment"})))),
-                        React.createElement('div', {className: "panel panel-warning"},
-                                React.createElement('div', {className: "panel-heading"}, "Variants"),
-                                    React.createElement('div', {className: "panel-body"},
-                                        React.createElement('div', {className: 'container', id: 'variants'},
-                                            React.createElement(this.contentUpdateProject, {panelType: "variants"})))),
-                        React.createElement('div', {className: "panel panel-danger"},
-                                React.createElement('div', {className: "panel-heading"}, "Annotation"),
-                                    React.createElement('div', {className: "panel-body"},
-                                        React.createElement('div', {className: 'container', id: 'annotation'},
-                                            React.createElement(this.contentUpdateProject, {panelType: "annotation"})))),
-                        React.createElement('div', {className: "panel panel-success"},
-                                React.createElement('div', {className: "panel-heading"}, "Expression"),
-                                    React.createElement('div', {className: "panel-body"},
-                                        React.createElement('div', {className: 'container', id: 'expression'},
-                                            React.createElement(this.contentUpdateProject, {panelType: "expression"})))),
-                        React.createElement('div', {className: "panel panel-info"},
-                                React.createElement('div', {className: "panel-heading"}, "Diferential expression"),
-                                    React.createElement('div', {className: "panel-body"},
-                                        React.createElement('div', {className: 'container', id: 'difExpression'},
-                                            React.createElement(this.contentUpdateProject, {panelType: "difExpression"})))))
+                React.createElement('label', {for: 'sequenceBtn'}, 'Reference sequence: '),
+                React.createElement('div', {className: 'container', id: 'sequence'},
+                        React.createElement(this.contentUpdateProject, {panelType: "sequence"})),
+                React.createElement('br'),
+                React.createElement('label', {for: 'alignmentBtn'}, 'Coverage depth: '),
+                React.createElement('div', {className: 'container', id: 'alignment'},
+                        React.createElement(this.contentUpdateProject, {panelType: "alignment"})),
+                React.createElement('br'),
+                React.createElement('label', {for: 'variantsBtn'}, 'SNP density: '),
+                React.createElement('div', {className: 'container', id: 'variants'},
+                        React.createElement(this.contentUpdateProject, {panelType: "variants"})),
+                React.createElement('br'),
+                React.createElement('label', {for: 'annotationBtn'}, 'Annotation: '),
+                React.createElement('div', {className: 'container', id: 'annotation'},
+                        React.createElement(this.contentUpdateProject, {panelType: "annotation"})),
+                React.createElement('br'),
+                React.createElement('label', {for: 'expressionBtn'}, 'Genes expression: '),
+                React.createElement('div', {className: 'container', id: 'expression'},
+                        React.createElement(this.contentUpdateProject, {panelType: "expression"})),
+                React.createElement('br'),
+                React.createElement('label', {for: 'difExpressionBtn'}, 'Differential expression: '),
+                React.createElement('div', {className: 'container', id: 'difExpression'},
+                        React.createElement(this.contentUpdateProject, {panelType: "difExpression"})))
                 );
     }
 });
